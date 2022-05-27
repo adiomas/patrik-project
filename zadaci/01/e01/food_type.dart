f5e9334bccf2b140575ed770175c4070a7d8a9fa
Patrik Pralas <patrik.pralas@q.agency>
2022-05-27 09:23:26 +0200

    files adjustments

--- zadaci/01/e01/food_type.dart
+++ zadaci/01/e01/food_type.dart
@@ -1,4 +1,3 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 class FoodType {
   String? name;
   int? protein;
@@ -11,31 +10,4 @@ class FoodType {
     this.carbs,
     this.fat,
   );
-
-  set foodName(String name) {
-    foodName = name;
-  }
-
-  set foodProtein(int protein) {
-    foodProtein = protein;
-  }
-
-  set foodCarbs(int carbs) {
-    foodCarbs = carbs;
-  }
-
-  set foodFat(int fat) {
-    foodFat = fat;
-  }
-
-  @override
-  FoodType.toString(String name, int protein, int carbs, int fat) {
-    print('$name has  p- $protein %, c- $carbs %, f - $fat % ');
-  }
-}
-
-void main() {
-  final banana = FoodType.toString('banana', 4, 93, 4);
-
-  print(banana);
 }
