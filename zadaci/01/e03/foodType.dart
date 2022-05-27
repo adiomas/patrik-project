ef44589ab010f5774a8e153c2512417d2226b908
Patrik Pralas <patrik.pralas@q.agency>
2022-05-27 09:24:53 +0200

    files adjustments eo3

--- zadaci/01/e03/foodType.dart
+++ zadaci/01/e03/foodType.dart
@@ -1,1 +1,34 @@
+class FoodType {
+  String? name;
+  int? protein;
+  int? carbs;
+  int? fat;
 
+  FoodType(
+    this.name,
+    this.protein,
+    this.carbs,
+    this.fat,
+  );
+
+  set foodName(String name) {
+    foodName = name;
+  }
+
+  set foodProtein(int protein) {
+    foodProtein = protein;
+  }
+
+  set foodCarbs(int carbs) {
+    foodCarbs = carbs;
+  }
+
+  set foodFat(int fat) {
+    foodFat = fat;
+  }
+
+  @override
+  FoodType.toString(String name, int protein, int carbs, int fat) {
+    print('$name has  p- $protein %, c- $carbs %, f - $fat % ');
+  }
+}
--- zadaci/01/e03/main.dart
+++ zadaci/01/e03/main.dart
@@ -1,5 +1,11 @@
-
+import 'foodType.dart';
 // void main() {
 //   FoodType banana = FoodType(name: 'banana', protein: 4, carbs: 93, fat: 3);
 //   print(banana.toString());
 // }
+
+void main() {
+  final banana = FoodType.toString('banana', 4, 93, 4);
+
+  print(banana);
+}
