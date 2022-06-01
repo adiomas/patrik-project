ce23615d22febcb2dbe8df30588f31d7f2eb3442
Patrik Pralas <patrik.pralas@q.agency>
2022-06-01 15:26:27 +0200

    e02, task1 commit

--- zadaci/02/e01/dessert.dart
+++ zadaci/02/e01/dessert.dart
@@ -1,1 +1,24 @@
-class Desert {}
+class Dessert {
+  String? name;
+  double? weight;
+  int? calories;
+
+  Dessert(this.name, this.weight, this.calories);
+
+  String? get getName => this.name;
+
+  set setName(String? name) => this.name = name;
+
+  get getWeight => this.weight;
+
+  set setWeight(weight) => this.weight = weight;
+
+  get getCalories => this.calories;
+
+  set setCalories(calories) => this.calories = calories;
+
+  @override
+  String toString() {
+    return ('${getName} is ${getWeight} heavy and has ${getCalories}! ');
+  }
+}
