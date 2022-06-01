2b362633a5ec4ee7ad454e953daf02c71378b6b6
Adrijan <121095adioma@gmail.com>
2022-06-01 15:00:31 +0200

    added more tasks

---
+++ zadaci/02/e04/main.dart
@@ -0,0 +1,32 @@
+main() {
+  // List<Teacher> teachers = [];
+  // final teacher1 = Teacher(
+  //     name: "Adrijan",
+  //     surname: "Omicevic",
+  //     age: 26,
+  //     email: "adrijan.omicevic@q.agency",
+  //     subject: "Flutter",
+  //     salary: 30000);
+
+  // final teacher2 = Teacher(
+  //     name: "Dominik",
+  //     surname: "Mesek",
+  //     age: 24,
+  //     email: "dominik.mesek@q.agency",
+  //     subject: "Flutter & Dart",
+  //     salary: 20000);
+
+  // teachers.add(teacher1);
+  // teachers.add(teacher2);
+
+  // print(teachers.toString());
+
+  // //[name: Adrijan, surname: Omicevic, age: 26, email: adrijan.omicevic@q.agency, subject: Flutter, salary: 30000.0,
+  //// name: Dominik, surname: Mesek, age: 24, email: dominik.mesek@q.agency, subject: Flutter & Dart, salary: 20000.0]
+
+//   Teacher.increaseTeachersSalaries(15, teachers);
+
+//   print(teachers.toString());
+//   // [name: Adrijan, surname: Omicevic, age: 26, email: adrijan.omicevic@q.agency, subject: Flutter, salary: 34500.0,
+//   // name: Dominik, surname: Mesek, age: 24, email: dominik.mesek@q.agency, subject: Flutter & Dart, salary: 23000.0]
+}
