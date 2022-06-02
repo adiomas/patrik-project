bbee7ac781bd5b84c2cca594ce4443851271ea87
Patrik Pralas <patrik.pralas@q.agency>
2022-06-02 10:51:49 +0200

    created person

--- zadaci/02/e03/person.dart
+++ zadaci/02/e03/person.dart
@@ -1,1 +1,30 @@
+class Person {
+  String name = '';
+  String surname = '';
+  int age = 0;
 
+  Person(this.name, this.surname, this.age);
+
+  String get getName => this.name;
+
+  set setName(String name) => this.name = name;
+
+  String get getSurname => this.surname;
+
+  set setSurname(String name) => this.surname = surname;
+
+  get getAge => this.age;
+
+  set setAge(int age) => this.age = age;
+
+  String toString() {
+    return ('${getName} ${getSurname} is  ${getAge} old');
+  }
+
+  String? equals(Person) {
+    if (Person == Person) {
+      return ('Same person');
+    } else
+      return ('Not same person');
+  }
+}
